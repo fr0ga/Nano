3c8b552005d022157b9e15364215117040413def
fr0ga <fr0ga@yadnex.ru>
2013-12-22 23:40:09 +0400

    132

--- src/NAN.java
+++ src/NAN.java
@@ -6,4 +6,15 @@
  * To change this template use File | Settings | File Templates.
  */
 public class NAN {
+    public static void main(String[] args) {
+        long timeout= System.currentTimeMillis();
+        long timeNano = System.nanoTime();
+        for (int i = 0; i < 1000000; i++) {
+            System.out.println(i);
+        }
+        timeout = System.currentTimeMillis() - timeout;
+        timeNano =  System.nanoTime() - timeNano;
+        //System.out.println("Затраченное время: " + timeout + " ms");
+        System.out.println("Затраченное время: " + timeNano + " ns");
+    }
 }
