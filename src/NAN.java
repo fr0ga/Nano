0260bf76127ce1389d4103454f9a63af3b7ef9db
fr0ga <fr0ga@yadnex.ru>
2013-12-22 23:45:10 +0400

    123

--- src/NAN.java
+++ src/NAN.java
@@ -1,16 +1,3 @@
-/**
- * Created with IntelliJ IDEA.
- * User: fr0ga
- * Date: 22.12.13
- * Time: 23:37
- * To change this template use File | Settings | File Templates.
- */
-
-class Na {
-
-
-}
-
 class Nano {
     public static void main(String[] args) {
         long timeout= System.currentTimeMillis();
